3b1b165ef443504541a5ab4d572dc96fa3b68818
Robin Radic <robin@radic.nl>
2016-06-19 17:32:31 +0200

    debugbar fix

--- resources/assets/scripts/vendor.js
+++ resources/assets/scripts/vendor.js
@@ -35594,6 +35594,54 @@ if (typeof jQuery === 'undefined') {
 
 	return $.fn.jstree;
 }));
+/*
+ * Copyright (c) 2016. Robin Radic.
+ *
+ * The license can be found in the package and online at https://radic.mit-license.org.
+ *
+ * @copyright Copyright 2016 (c) Robin Radic
+ * @license https://radic.mit-license.org The MIT License
+ */
+
+(function () {
+
+    if ( typeof self === 'undefined' || ! self.Prism || ! self.document ) {
+        return;
+    }
+
+// The languages map is built automatically with gulp
+    var Languages = /*languages_placeholder[*/{"html": "HTML", "xml": "XML", "svg": "SVG", "mathml": "MathML", "css": "CSS", "clike": "C-like", "javascript": "JavaScript", "abap": "ABAP", "actionscript": "ActionScript", "apacheconf": "Apache Configuration", "apl": "APL", "applescript": "AppleScript", "asciidoc": "AsciiDoc", "aspnet": "ASP.NET (C#)", "autoit": "AutoIt", "autohotkey": "AutoHotkey", "basic": "BASIC", "csharp": "C#", "cpp": "C++", "coffeescript": "CoffeeScript", "css-extras": "CSS Extras", "fsharp": "F#", "glsl": "GLSL", "http": "HTTP", "inform7": "Inform 7", "json": "JSON", "latex": "LaTeX", "lolcode": "LOLCODE", "matlab": "MATLAB", "mel": "MEL", "nasm": "NASM", "nginx": "nginx", "nsis": "NSIS", "objectivec": "Objective-C", "ocaml": "OCaml", "parigp": "PARI/GP", "php": "PHP", "php-extras": "PHP Extras", "powershell": "PowerShell", "protobuf": "Protocol Buffers", "jsx": "React JSX", "rest": "reST (reStructuredText)", "sas": "SAS", "sass": "Sass (Sass)", "scss": "Sass (Scss)", "sql": "SQL", "typescript": "TypeScript", "vhdl": "VHDL", "vim": "vim", "wiki": "Wiki markup", "yaml": "YAML"}/*]*/;
+    Prism.hooks.add('before-highlight', function (env) {
+        var pre = env.element.parentNode;
+        if ( ! pre || ! /pre/i.test(pre.nodeName) ) {
+            return;
+        }
+        var language = pre.getAttribute('data-language') || Languages[env.language] || (env.language.substring(0, 1).toUpperCase() + env.language.substring(1));
+
+        /* check if the divs already exist */
+        var sib = pre.previousSibling;
+        var div, div2;
+        if ( sib && /\s*\bprism-show-language\b\s*/.test(sib.className) &&
+            sib.firstChild &&
+            /\s*\bprism-show-language-label\b\s*/.test(sib.firstChild.className) ) {
+            div2 = sib.firstChild;
+        } else {
+            div = document.createElement('div');
+            div2 = document.createElement('div');
+
+            div2.className = 'prism-show-language-label';
+
+            div.className = 'prism-show-language';
+            div.appendChild(div2);
+
+            if ( pre.parentNode !== null ) pre.parentNode.insertBefore(div, pre);
+        }
+
+        div2.innerHTML = language;
+    });
+
+})();
+
 /* http://prismjs.com/download.html?themes=prism&languages=markup+css+clike+javascript+apacheconf+bash+coffeescript+css-extras+git+handlebars+ini+jade+json+less+markdown+php+php-extras+scss+smarty+sql+stylus+twig+typescript+yaml&plugins=line-numbers+show-language */
 var _self = (typeof window !== 'undefined')
     ? window   // if in browser
@@ -37648,11 +37696,12 @@ Prism.languages.yaml = {
             div2 = document.createElement('div');
 
             div2.className = 'prism-show-language-label';
-
             div.className = 'prism-show-language';
             div.appendChild(div2);
 
-            pre.parentNode.insertBefore(div, pre);
+            if(pre.parentNode !== null) {
+                pre.parentNode.insertBefore(div, pre);
+            }
         }
 
         div2.innerHTML = language;
@@ -38213,41 +38262,3 @@ Prism.hooks.add('before-highlight', function (env) {
 });
 
 }());
-(function(){
-
-if (typeof self === 'undefined' || !self.Prism || !self.document) {
-	return;
-}
-
-// The languages map is built automatically with gulp
-var Languages = /*languages_placeholder[*/{"html":"HTML","xml":"XML","svg":"SVG","mathml":"MathML","css":"CSS","clike":"C-like","javascript":"JavaScript","abap":"ABAP","actionscript":"ActionScript","apacheconf":"Apache Configuration","apl":"APL","applescript":"AppleScript","asciidoc":"AsciiDoc","aspnet":"ASP.NET (C#)","autoit":"AutoIt","autohotkey":"AutoHotkey","basic":"BASIC","csharp":"C#","cpp":"C++","coffeescript":"CoffeeScript","css-extras":"CSS Extras","fsharp":"F#","glsl":"GLSL","http":"HTTP","inform7":"Inform 7","json":"JSON","latex":"LaTeX","lolcode":"LOLCODE","matlab":"MATLAB","mel":"MEL","nasm":"NASM","nginx":"nginx","nsis":"NSIS","objectivec":"Objective-C","ocaml":"OCaml","parigp":"PARI/GP","php":"PHP","php-extras":"PHP Extras","powershell":"PowerShell","protobuf":"Protocol Buffers","jsx":"React JSX","rest":"reST (reStructuredText)","sas":"SAS","sass":"Sass (Sass)","scss":"Sass (Scss)","sql":"SQL","typescript":"TypeScript","vhdl":"VHDL","vim":"vim","wiki":"Wiki markup","yaml":"YAML"}/*]*/;
-Prism.hooks.add('before-highlight', function(env) {
-	var pre = env.element.parentNode;
-	if (!pre || !/pre/i.test(pre.nodeName)) {
-		return;
-	}
-	var language = pre.getAttribute('data-language') || Languages[env.language] || (env.language.substring(0, 1).toUpperCase() + env.language.substring(1));
-
-	/* check if the divs already exist */
-	var sib = pre.previousSibling;
-	var div, div2;
-	if (sib && /\s*\bprism-show-language\b\s*/.test(sib.className) &&
-		sib.firstChild &&
-		/\s*\bprism-show-language-label\b\s*/.test(sib.firstChild.className)) {
-		div2 = sib.firstChild;
-	} else {
-		div = document.createElement('div');
-		div2 = document.createElement('div');
-
-		div2.className = 'prism-show-language-label';
-
-		div.className = 'prism-show-language';
-		div.appendChild(div2);
-
-		pre.parentNode.insertBefore(div, pre);
-	}
-	
-	div2.innerHTML = language;
-});
-
-})();
--- src/Addon/Jira/Api/Api.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Codex\Addon\Jira\Api;
-
-class Api extends \chobie\Jira\Api
-{
-
-}
