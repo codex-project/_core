cafc9f9e37c791fa55a7fdc94ebe8461f82aec97
Robin Radic <robin@radic.nl>
2017-01-10 12:28:49 +0100

    upd

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<c-footer ref="footer">
+    <p>Copyright {{ date('Y') }} - {{ config('codex.display_name') }}.</p>
+    <p>Brought to you by Codex - a Open Source documentation platform </p>
+</c-footer>
